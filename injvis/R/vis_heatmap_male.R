94ab569c2c428736822b0f9172a27d30059a3800
alexandraD03 <alexandra.dooley@students.mq.edu.au>
2025-04-28 23:36:32 +1000

    Code vis_heatmap_male
    Worked on code for a heatmap visual function for the male human body.

--- injvis/R/vis_heatmap_male.R
+++ injvis/R/vis_heatmap_male.R
@@ -1,18 +1,106 @@
-vis_heatmap_male <- function(data, body_view, colour_palette, legend){
+vis_heatmap_male <- function(data, body_view, colour_palette = "YIOrRd", legend){
   library(grid)
   library(ggplot2)
   library(recolorize)
   library(gridExtra)
   library(OpenImageR)
   library(magick)
+  library(RColorBrewer)
 
   if(body_view == "front"){
-    body <- image_read("/Users/alexandradooley/Desktop/Uni/Year 4/COMP3850/COMP3850-Group-15-R-Package/injvis/Body Images/body_male_front.jpg")
+    body <- image_read("/Users/alexandradooley/Desktop/Uni/Year 4/COMP3850/COMP3850-Group-15-R-Package/injvis/Body Images/body_male_front.png")
+    plot(body)
+
+    overlay <- image_blank(width = 20,
+                           height = 20,
+                           color = "#FF000080") # Red with 50% opacity
+
+    # Composite the overlay on top
+    img_colored <- image_composite(body, overlay, operator = "atop")
+
+    # Plot the result
+    plot(img_colored)
+
+    #heat.colors(4, rev = FALSE))
 
     # # load image
     # body <- readImage(img_path, resize = NULL, rotate = NULL)
     # body <- load.image(img_path)
     # img <- readJPEG("/injvis/Body Images/body_male_front.jpg")
+
+    # Get image size
+    info <- image_info(body)
+    img_width <- info$width
+    img_height <- info$height
+
+    # Create blank transparent layer
+    heatmap_layer <- image_blank(width = img_width, height = img_height, color = "none")
+
+    # Define a color palette (blue → red)
+   # palette_func <- colorRampPalette(c("blue", "green", "yellow", "red"))
+      #heat.colors(4, rev = FALSE)
+      #colorRampPalette(c("blue", "green", "yellow", "red"))
+    palette_colours <- heat.colors(100, rev = FALSE)
+      #palette_func(100)
+
+    body_regions <- data.frame(
+      region = c("Head and neck", "Upper limb", "Trunk", "Lower limb", "Unspecified"),
+      # x_start = c(0.4, 0.0, 0.6, 0.4, 0.4, 0.5),
+      # x_end   = c(0.6, 0.4, 1.0, 0.6, 0.5, 0.6),
+      # y_start = c(0.2, 0.2, 0.2, 0.4, 0.6, 0.6),
+      # y_end   = c(0.4, 0.5, 0.5, 0.6, 1.0, 1.0),
+      # heat_value = c(0.8, 0.5, 0.5, 0.7, 0.6, 0.6)
+    )
+
+    body_areas <- data.frame(
+      region = c("Head", "Neck", "Shoulder", "Upper arm", "Elbow", "Forearm", "Wrist", "Hand",
+                 "Chest", "Thoracic spine", "Lumbosacral", "Abdomen", "Hip/groin",
+                 "Thigh", "Knee", "Lower leg", "Ankle", "Foot", "Region unspecified"),
+      # x_start = c(0.4, 0.0, 0.6, 0.4, 0.4, 0.5),
+      # x_end   = c(0.6, 0.4, 1.0, 0.6, 0.5, 0.6),
+      # y_start = c(0.2, 0.2, 0.2, 0.4, 0.6, 0.6),
+      # y_end   = c(0.4, 0.5, 0.5, 0.6, 1.0, 1.0),
+      # heat_value = c(0.8, 0.5, 0.5, 0.7, 0.6, 0.6)
+    )
+
+    # Now for each region, create a colored transparent rectangle
+    for (i in 1:nrow(regions)) {
+      r <- regions[i,]
+
+      # Calculate pixel positions
+      x_start_px <- round(r$x_start * img_width)
+      x_end_px <- round(r$x_end * img_width)
+      y_start_px <- round(r$y_start * img_height)
+      y_end_px <- round(r$y_end * img_height)
+
+      width_px <- x_end_px - x_start_px
+      height_px <- y_end_px - y_start_px
+
+      # Color based on heat value
+      color_index <- round(r$heat_value * 99) + 1
+      color <- adjustcolor(palette_colors[color_index], alpha.f = 0.5)
+
+      # Create the rectangle
+      rect <- image_blank(width = width_px, height = height_px, color = col2hex(color))
+
+      # Composite rectangle into the heatmap layer
+      heatmap_layer <- image_composite(
+        heatmap_layer, rect,
+        offset = paste0("+", x_start_px, "+", y_start_px)
+      )
+    }
+
+    # Composite heatmap onto body
+    final_img <- image_composite(body_img, heatmap_layer, operator = "atop")
+
+    # Show result
+    plot(final_img)
+
+# Composite heatmap onto body
+final_img <- image_composite(body_img, heatmap_layer, operator = "atop")
+
+# Show result
+plot(final_img)
 
   } else if (body_view == "back") {
     body <- image_read("/Users/alexandradooley/Desktop/Uni/Year 4/COMP3850/COMP3850-Group-15-R-Package/injvis/Body Images/body_male_back.jpg")
