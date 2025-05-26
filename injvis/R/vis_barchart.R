79a947aa166283f07cc20a8bcb36210b944567aa
BriceThu <brice.thu@gmail.com>
2025-05-27 04:50:32 +1000

    added colour blind option to bar chart
    - Users can use a colour blind mode
    - Users can specify what colour map from virdis libraby

--- injvis/R/vis_barchart.R
+++ injvis/R/vis_barchart.R
@@ -3,14 +3,21 @@
 #' Generate a bar chart to show the frequency of sports injuries per tissue and pathology type.
 #'
 #' @param data Sports injury data, with columns Tissue, Pathology.Type and Frequency
+#' @param colurblond description
+#' @param colourOption description
 #'
 #' @return Coloured barchart of sports injury data at tissue and pathology type level
 #' @export
 #'
-#' @examples vis_barchart(injuryTissuePathTable)
-vis_barchart <- function(data) {
+#' @examples
+#' vis_barchart(injuryTissuePathTable)
+#' vis_barchart(injuryTissuePathTable, colourblind = TRUE)
+#' vis_barchart(injuryTissuePathTable, colourblind = TRUE, colourOption = "D")
+#'
+vis_barchart <- function(data, colourblind = FALSE, colourOption = "A") {
   library(ggplot2)
   library(dplyr)
+  library(viridis)
 
   data <- data %>%
     mutate(Tissue = factor(Tissue,
@@ -25,6 +32,10 @@ vis_barchart <- function(data) {
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
           legend.position = "bottom") +
     scale_y_continuous(breaks = seq(0, max(data$Frequency), by = 5))
+
+  if(colourblind) {
+    barchart <- barchart + scale_fill_viridis_d(option = colourOption)
+  }
 
   barchart
 }
